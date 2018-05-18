cc2d8bfc817ef84aabc58182af697fb8f4be93d5
mozn-lz <mozn.lozn2000@gmail.com>
2018-05-18 11:21:15 +0200

    x ap updated

--- strcpy.c
+++ strcpy.c
@@ -15,16 +15,18 @@ int		ft_strlen(char  *str)
 }
 /*	end TEST functions	*/
 
-char	*ft_strcpy(char *src, char *dest)
+char	*ft_strcpy(char *dest, const char *src)
 {
+	/*	works	*/
 	int i;
 	
 	i = 0;
-	while (src)
+
+	while (src[i] != '\0')
 	{
-		src[i] = dest[i];
+		dest[i] = src[i];
 		i++;
-	}	\
+	}
 	dest[i] = '\0';
 	return (dest);
 }
@@ -34,10 +36,11 @@ char	*ft_strncpy(char *dest, const char *src, size_t len)
 	int i;
 	
 	i = 0;
-	while (src)
+	while ((src[i] != '\0') && len > 1)
 	{
-	//	src[i] = dest[i];
+		dest[i] = src[i];
 		i++;
+		len--;
 	}
 	dest[i] = '\0';
 	return (dest);
@@ -73,8 +76,10 @@ int		main(void)
 	char s1[] = "lord";
 	char s2[] = "Chaos";
 
-	ft_strcat(s1, s2);
-	printf("string: %s", s1);
+//	printf("strcat %s\n", ft_strcat(s1, s2));
+	printf("strcpy: %s\n", ft_strcpy(s1, s2));
+	printf("strncpy: %s\n", ft_strncpy(s1, s2, sizeof(s2)));
+//	printf("string: %s\n", s1);
 
 	return (0);
 }
