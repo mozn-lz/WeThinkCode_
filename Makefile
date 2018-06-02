981aa3fe00190954a5a7e63a4469f196a3cf2677
moeketsane SEFAKO <msefako@c3r7s7.wethinkcode.co.za>
2018-06-02 21:17:58 +0200

    @ functiona

--- Makefile
+++ Makefile
@@ -7,19 +7,19 @@ ft_isascii.c	ft_memchr.c	ft_memset.c	ft_strchr.c ft_strdup.c	ft_strnstr.c\
 ft_atoi.c ft_isdigit.c ft_memcmp.c	ft_putchar.c	ft_strclr.c	ft_striter.c	ft_strncat.c	ft_strrchr.c\
 ft_bzero.c ft_isprint.c ft_memcpy.c	ft_putnbr.c	ft_strcmp.c ft_striteri.c ft_strncmp.c	ft_strstr.c\
 ft_isalnum.c ft_memalloc.c	ft_memdel.c	ft_putstr.c	ft_strcpy.c	ft_strlcat.c ft_strncpy.c ft_tolower.c\
-RM: rm
+RM=rm
 
 NAME=libft.a
 
 OBJ=$(SRC:.c=.o)
 
+all: $(NAME)
 	$(CC) $(FLAGS) -c $(HEAD) $(SRC)
 	ar rc $(NAME) $(OBJ)
 	ranlib $(NAME)
 
-all: $(NAME)
 
-clean:
+clean: $(RM)
 	$(RM) $(OBJ)
 
 fclean: clean
--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: msefako <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/01 16:45:04 by msefako           #+#    #+#             */
-/*   Updated: 2018/06/02 15:17:34 by msefako          ###   ########.fr       */
+/*   Updated: 2018/06/02 21:16:25 by msefako          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,33 @@ char	*ft_itoa(int n)
 	if (n < 0)
 	{
 		n *= -1;
-		str[i++] == '-';
+		str[i++] = '-';
 	}
 	if (n >= 10)
-		ft_itoa(n/10);
-	if (n )
+		ft_itoa(n /= 10);
+	if (n < 10)
+		str[i++] = (n + '0');
+	ft_putchar(n * 10 + '0');
 	return (str);
+}
+
+char ita(int num)
+{
+	char *str;
+	int i = 0;
+	 while (num != 0)
+	 {
+        int rem = num % 10;
+        str[i++] = (rem > 9)? (rem-10) + 'A' : rem + '0';
+        num = num/10;
+    }
+	 return (*str);
 }
 
 int		main(void)
 {
-	printf("%s\n", ft_itoa(30));
+	printf("%d\n", ita(350));
+//	printf("%s\n", ft_itoa(350));
+//	printf("%d\n", (350 + '0'));
 	return (0);
 }
