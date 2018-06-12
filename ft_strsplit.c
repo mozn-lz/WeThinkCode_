4b79e4fe973591e361d5dfca5f2d69d776b7f35a
moeketsane SEFAKO <msefako@c3r7s7.wethinkcode.co.za>
2018-06-12 10:58:28 +0200

    have to commit to undo

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,13 +6,13 @@
 /*   By: msefako <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/04 19:24:20 by msefako           #+#    #+#             */
-/*   Updated: 2018/06/05 18:01:13 by msefako          ###   ########.fr       */
+/*   Updated: 2018/06/12 10:32:12 by msefako          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	**ft_strsplit(const char*s, char c)
+char	**ft_strsplit(const char *s, char c)
 {
 	int		i;
 	int		j;
